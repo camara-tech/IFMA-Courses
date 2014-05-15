7d16e893fbb903aff632228c60dabc01f91f6c94
Jonathan Camara <jcamara@ifma.org>
2014-05-15 16:15:36 -0500

    updated the search form template to work properly

--- template-part-search.php
+++ template-part-search.php
@@ -4,7 +4,7 @@
 
 
 <!-- search -->
-<form role="search" action="/wordpress/courses" method="get" class="searchform">
+<form role="search" action="/wordpress/" method="get" class="searchform">
     <div>
         <label class="screen-reader-text" for="s">Search Courses:</label>
         <input type="text" name="s" id="s" placeholder="Search Courses"/>
