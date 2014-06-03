fe2333a7372bff36245151ddc4ac257feaaddb59
Jonathan Camara <jcamara@ifma.org>
2014-06-03 15:54:44 -0500

    added Grunt, started work on data model in js

---
+++ Gruntfile.js
@@ -0,0 +1,17 @@
+module.exports = function(grunt) {
+
+    // Project configuration.
+    
+    grunt.initConfig({
+        pkg: grunt.file.readJSON('package.json'),
+        jshint: {
+            all:['Gruntfile.js']
+
+        } 
+    });
+    
+grunt.loadNpmTasks('grunt-contrib-jshint');
+
+grunt.registerTask('default', ['jshint']);
+};
+
