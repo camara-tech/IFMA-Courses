b4e711c9bfd921ebba00033c1e558ebf6c51fab3
Jonathan Camara <jcamara@ifma.org>
2014-06-06 10:56:54 -0500

    updated grunt with watch task, fixed linting errors

--- Gruntfile.js
+++ Gruntfile.js
@@ -21,7 +21,8 @@ module.exports = function(grunt) {
         sass:{
             dist: {
                 options:{
-                    style:"compact"
+                    style:"compressed",
+                    banner:"/* Theme Name: IFMA Courses Theme\n URI: http://github.com/italy-portugal/IFMA-Courses \n Description: Theme for IFMA Courses Website \n Author: Jonathan Camara \n Author URI: http://www.ifma.org \n Version: 0.0.4 \n Tags: IFMA, white, bootstrap, responsive \n GitHub Theme URI: https://github.com/italy-portugal/IFMA-Courses \n GitHub Branch: develop \n */"
                 },
                 files:{
                     "./style.css":"./dev/scss/style.scss"
@@ -29,6 +30,33 @@ module.exports = function(grunt) {
             }
         },
         watch:{
+          config:{
+            files:['Gruntfile.js'],
+            tasks:['jshint'],
+            options:{
+              reload:true
+            }
+          },
+          scripts:{
+            files: ['./dev/js/*.js'],
+            tasks: ['jshint','uglify'],
+            options:{
+              livereload:true
+            } 
+          },
+          css:{
+            files:['./dev/scss/*.scss'],
+            tasks:['sass'],
+            options:{
+              livereload: true
+            }
+          },
+          php:{
+            files:['./templates/*.php','*.php'],
+            options:{
+              livereload: true
+            }
+          }
         }
     });
     
@@ -37,9 +65,20 @@ grunt.loadNpmTasks('grunt-contrib-uglify');
 grunt.loadNpmTasks('grunt-uncss');
 grunt.loadNpmTasks('grunt-concurrent');
 grunt.loadNpmTasks('grunt-contrib-csslint');
-grunt.loadNpmTasks('grunt-contrib-sass');
+grunt.loadNpmTasks('grunt-contrib-sass'); //fully configured, may change the banner option in order to make compression easier.
 grunt.loadNpmTasks('grunt-contrib-watch');
 
+
+// grunt tasks: default, watch, dist, deploy
+
+// default: run through all the configured tasks once
 grunt.registerTask('default', ['jshint', 'uglify','sass']);
+
+// watch: run tasks when a file change and live reload if possible
+grunt.registerTask('devel',['watch']);
+
+// dist: generate a non-development version that can be distributed separate from the codebase
+
+// deploy: generate the correct files and push up to Github
 };
 
--- header.php
+++ header.php
@@ -1,3 +1,12 @@
+<!DOCTYPE html>
+<html <?php language_attributes(); ?> >
+<head>
+<meta charset="<?php bloginfo('charset'); ?>">
+<title><?php wp_title('|',true,'right'); ?></title>
+<link rel="stylesheet" href="<?php echo get_stylesheet_uri(); ?>" type="text/css" media="screen" />
+<?php wp_head(); ?>
+</head>
+  <body <?php body_class(); ?>>
 <?php 
 get_template_part('templates/template','micro_nav');
 get_template_part('templates/template','utility');
