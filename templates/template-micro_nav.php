59521273cc6a4c035a17a871131c18c17e9938b2
Jonathan Camara <jcamara@ifma.org>
2014-06-10 15:48:18 -0500

    updated micro nav component

--- templates/template-micro_nav.php
+++ templates/template-micro_nav.php
@@ -1,6 +1,6 @@
 <div class="micro_nav">
 <div class="container">
-<?php wp_nav_menu( array( 'theme_location' => 'micro-nav-menu')); ?>
+<?php wp_nav_menu( array( 'theme_location' => 'micro-nav-menu', 'items_wrap' => '<ul id="%1$s" class="%2$s"><li><strong>IFMA Network:</strong></li>%3$s</ul>')); ?>
 
 <!-- Menu for IFMA Network -->
 <!-- Menu for Learn -->
@@ -8,4 +8,3 @@
 <!-- Menu for Advance -->
 </div>
  </div>
-
