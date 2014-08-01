8b995331148a85f6532643ecb6c45d4116da9053
Jonathan Camara <jcamara@ifma.org>
2014-08-01 15:17:32 -0500

    added ability to sort ascending and descending.

--- templates/template-sort.php
+++ templates/template-sort.php
@@ -14,9 +14,25 @@ global $wp_query;
 
     if (isset($_GET['meta_value'])) {
     unset($query_string['meta_value']);
-    } ?>
+    }
+
+    if (isset($_GET['order'])) {
+        if ($_GET['order'] == "ASC"){
+            $query_string['order'] = "DESC";
+        } else {
+            $query_string['order'] = "ASC";
+        }
+    } else {
+        $query_string['order'] = "ASC";
+    }
+
+}
+    ?>
 <div class="filters">
     <p class="pull-left"><?php echo $wp_query->found_posts; ?> Courses found</p>
-    <p class="pull-right">Sort By: <a href="?<?php echo http_build_query($query_string)?>&meta_key=start_date&orderby=meta_value_num">Date</a> | <a href="?<?php echo http_build_query($query_string)?>&meta_key=map_location&orderby=meta_value">Location</a> | <a href="?<?php echo http_build_query($query_string) ?>&orderby=title">A-Z</a></p>
+    <p class="pull-right">Sort By:
+        <a href="?<?php echo http_build_query($query_string)?>&meta_key=start_date&orderby=meta_value_num">Date</a> <?php if ($query_string['order'] == "ASC"){ echo "<i class='fa fa-sort-desc'></i>"; } else { echo "<i class='fa fa-sort-asc'></i>"; ?> |
+        <a href="?<?php echo http_build_query($query_string)?>&meta_key=map_location&orderby=meta_value">Location</a> <?php if ($query_string['order'] == "ASC"){ echo "<i class='fa fa-sort-desc'></i>"; } else { echo "<i class='fa fa-sort-asc'></i>"; }?> |
+        <a href="?<?php echo http_build_query($query_string) ?>&orderby=title">A-Z</a><?php if ($query_string['order'] == "ASC"){ echo "<i class='fa fa-sort-desc'></i>"; } else { echo "<i class='fa fa-sort-asc'></i>";} ?></p>
 </div>
 <?php } ?>
