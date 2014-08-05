4d15989bbbcada747550668647cb9c09004ece7a
Jonathan Camara <jcamara@ifma.org>
2014-08-05 10:39:11 -0500

    Made sure all the relevant information about the course is actually on the page

--- single-course.php
+++ single-course.php
@@ -5,12 +5,12 @@
     <div class="row">
     <?php //get_template_part('template-part', 'search'); ?>
         <div class="main-content">
-            <?php // theloop
-            if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
+            <!-- the loop -->
+            <?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
                 <div <?php post_class(); ?>>
-                    <?php if ($_SERVER['HTTP_REFERER']) {
-                        echo "<p class='search-return'><a href='javascript:history.go(-1)'><< Return to Search Results</a></p>";
-                    } ?>
+                    <?php if ($_SERVER['HTTP_REFERER']) { ?>
+                        <p class='search-return'><a href='javascript:history.go(-1)'><< Return to Search Results</a></p>
+                    <?php } ?>
                     <h2 class="page-header course-title"><?php the_title() ;?></h2>
                     <div class="sidebar col-lg-4">
                         <div class="course-credential center-block">
@@ -26,20 +26,34 @@
                             }
                             ?>
                         </div>
+                        <div class="course-contact">
+                            <h3>Contact:</h3>
+                            <?php $contact = get_field('contact'); ?>
+                            <p><?php echo get_field('contact_fname',$contact[0]->ID)." ".get_field('contact_lname',$contact[0]->ID); ?></p>
+                            <p><?php echo get_field('email',$contact[0]->ID); ?></p>
+                            <p><?php echo get_field('phone_number',$contact[0]->ID); ?></p>
+                        </div>
                         <div class="course-location">
                             <?php
                             //map stuff
                             $location = get_field('map_location');
                             if ($location) {
                                 $place = get_field('map_location', $location[0]->ID);
-                                //echo "<p> Address: {$place['address']} </p>";
-                                echo "<img src='http://maps.googleapis.com/maps/api/staticmap?markers=".$place['lat'].",".$place['lng']."&zoom=13&size=375x375&sensor=false' alt='map location'>";
+                                ?>
+                                <h3> Location: </h3>
+                                <?php
+                                echo "<p>".$place['address']."</p>";
+                                echo "<p><img src='http://maps.googleapis.com/maps/api/staticmap?markers=".$place['lat'].",".$place['lng']."&zoom=13&size=375x375&sensor=false' alt='map location'></p>";
                             } ?>
                         </div>
                     </div>
                     <div class="course-details">
                         <?php if (get_field('start_date')){ ?>
-                            <p><i class="fa fa-calendar"></i> <?php echo date('M d, Y',strtotime(get_field('start_date'))); ?></p>
+                            <?php if (get_field('end_date') > get_field('start_date')){ ?>
+                            <p><i class="fa fa-calendar"></i> <?php echo date('M d, y',strtotime(get_field('start_date')))." - ".date('M d, y',strtotime(get_field('start_date'))); ?></p>
+                            <?php } else { ?>
+                                <p><i class="fa fa-calendar"></i> <?php echo date('M d, Y',strtotime(get_field('start_date'))); ?></p>
+                            <?php } ?>
                         <?php } else { ?>
                             <p><i class="fa fa-calendar"></i> Anytime</p>
                         <?php } ?>
@@ -73,7 +87,16 @@
                                         $name = get_field('first_name',$instructor[0])." ".get_field('last_name',$instructor[0]);?>
                                     <p>Instructor: <?php echo $name; ?></p>
                                     <?php } ?>
-                                <p>CEU Level: <?php echo get_field("level"); ?></p>
+                                <p>Course Level: <?php echo get_field("level"); ?></p>
+                                <?php if (get_field('college_credits')) { ?>
+                                    <p>College Credit Hours: <?php echo get_field('college_credits') ?></p>
+                                <?php } ?>
+                                <?php if (get_field('number_of_ceu')) { ?>
+                                    <p>Number of CEU: <?php echo get_field('number_of_ceu'); ?>  </p>
+                                <?php } ?>
+                                <?php if (get_field("registration_url") != ""){ ?>
+                                <p><a class="btn btn-action pull-right" href="<?php echo get_field('registration_url'); ?>">Register Now</a></p>
+                                <?php } ?>
 
                             </div>
 
@@ -84,7 +107,7 @@
                             <?php the_content();
                             if(get_field("registration_url") != ""){
                                 ?>
-                            <a class="btn pull-right" href="<?php echo get_field('registration_url'); ?>">Register Now</a>
+                            <a class="btn btn-action pull-right" href="<?php echo get_field('registration_url'); ?>">Register Now</a>
                         <?php }?>
                             </div>
 
