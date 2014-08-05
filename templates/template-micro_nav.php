78e6db0b89b402ad39eb1e689a3b2e13a3088f45
Jonathan Camara <jcamara@ifma.org>
2014-08-05 09:11:28 -0500

    brought in code from boilerplate

--- templates/template-micro_nav.php
+++ templates/template-micro_nav.php
@@ -1,10 +1,38 @@
 <div class="micro_nav">
 <div class="container">
-<?php wp_nav_menu( array( 'theme_location' => 'micro-nav-menu', 'items_wrap' => '<ul id="%1$s" class="%2$s"><li><strong>IFMA Network:</strong></li>%3$s</ul>')); ?>
+
+<!-- <?php// wp_nav_menu( array( 'theme_location' => 'micro-nav-menu', 'link_after' => ' | ','items_wrap' => '<ul id="%1$s" class="%2$s"><li><strong>IFMA Network:</strong></li>%3$s</ul>')); ?> -->
 
 <!-- Menu for IFMA Network -->
 <!-- Menu for Learn -->
 <!-- Menu for Connect -->
 <!-- Menu for Advance -->
+<ul>
+                        <li><strong>IFMA Network:</strong></li>
+                        <li><a href="http://www.ifma.org">IFMA.org</a></li>
+                        <li>|</li>
+                        <li><a href="http://www.ifmacredentials.org">Credentials</a></li>
+                        <li>|</li>
+                        <li><a href="http://www.ifmacommunity.org">Community</a></li>
+                        <li>|</li>
+                        <li><a href="http://worldworkplace.ifma.org">World Workplace</a></li>
+                        <li>|</li>
+                        <li><a href="http://facilityfusion.ifma.org">Facility Fusion</a></li>
+                        <li>|</li>
+                        <li><a href="http://www.fmjonline.com">FMJ</a></li>
+                        <li>|</li>
+                        <li><a href="http://foundation.ifma.org">Foundation</a></li>
+                        <li><strong>Learn:</strong></li>
+                        <li><a href="http://www.ifma.org/education/calendar.htm"><img src="<?php echo get_template_directory_uri(); ?>/images/calendar-16px.png" alt="Calendar"></a></li>
+                        <li><a href="http://www.ifma.org/education/find-courses.htm"><img src="<?php echo get_template_directory_uri(); ?>/images/instructor-led-16px.png" alt="Courses"></a></li>
+                        <li><strong>Connect:</strong></li>
+                        <li><a href="http://www.facebook.com/InternationalFacilityManagementAssociation"><img src="<?php echo get_template_directory_uri(); ?>/images/facebook_thumb.png" alt="Facebook" width="16"></a></li>
+                        <li><a href="http://twitter.com/IFMA"><img src="<?php echo get_template_directory_uri(); ?>/images/twitter_thumb.png" alt="Twitter" width="16"></a></li>
+                        <li><a href="http://www.linkedin.com/groups?gid=38141"><img src="<?php echo get_template_directory_uri(); ?>/images/linkedin_thumb.png" alt="Linkedin" width="16"></a></li>
+                        <li><strong>Advance:</strong></li>
+                        <li><a href="http://www.ifmacredentials/fmp/"><img src="<?php echo get_template_directory_uri(); ?>/images/fmp-16px.png" alt="FMP"></a></li>
+                        <li><a href="http://www.ifmacredentials/cfm/"><img src="<?php echo get_template_directory_uri(); ?>/images/cfm-16px.png" alt="CFM"></a></li>
+                        <li><a href="http://www.ifmacredentials/sfp/"><img src="<?php echo get_template_directory_uri(); ?>/images/sfp-16px.png" alt="SFP"></a></li>
+                </ul>
 </div>
  </div>
