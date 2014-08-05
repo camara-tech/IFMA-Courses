bac3d209cbba2fde2d15c3519ad539ce5c5194a1
Jonathan Camara <jcamara@ifma.org>
2014-08-05 09:10:05 -0500

    added tagline

--- templates/template-header.php
+++ templates/template-header.php
@@ -1,6 +1,11 @@
 <div class="header">
 <div class="container">
-<a href="<?php echo site_url();  ?>"><div class="logo"><img src="<?php header_image(); ?>" alt="IFMA Logo" width="300"/></div> <h1>Facility Management <br /> Education</h1></a>
+<a href="<?php echo site_url();  ?>"><div class="logo"><img src="<?php header_image(); ?>" alt="IFMA Logo" width="300"/></div>
+<h1>Facility Management Education</h1></a>
+
+<div class=tagline>
+    <p>Empowering Facility Professionals Worldwide</p>
+</div>
 
 </div>
 </div>
