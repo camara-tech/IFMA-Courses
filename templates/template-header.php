81b3aa1f574ba733f146444c5b7e682110e8e0a2
Jonathan Camara <jcamara@ifma.org>
2014-09-02 14:59:04 -0500

    moved the h1 to be inside the logo div.

--- templates/template-header.php
+++ templates/template-header.php
@@ -1,7 +1,7 @@
 <div class="header">
 <div class="container">
-<a href="<?php echo site_url();  ?>"><div class="logo"><img src="<?php header_image(); ?>" alt="IFMA Logo" width="300"/></div>
-<h1>Facility Management Education</h1></a>
+<a href="<?php echo site_url();  ?>"><div class="logo"><img src="<?php header_image(); ?>" alt="IFMA Logo" width="300"/><h1>Facility Management Education</h1></div></a>
+
 
 <div class=tagline>
     <p>Empowering Facility Professionals Worldwide</p>
