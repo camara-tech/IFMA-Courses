a7324231dfd01fb6c30cea7cd967466b82c65b72
Jonathan Camara <jcamara@ifma.org>
2014-09-04 08:52:38 -0500

    Updated and cleaned up the section about FM

--- front-page.php
+++ front-page.php
@@ -1,5 +1,38 @@
 <?php //according to the wordpress codex, this is the default front-page for the website.
 get_header(); ?>
+
+<div class="about-fm">
+        <div class="about-fm-definition">
+
+          <h2>What is Facility Management?</h2>
+          <p>
+              Facility Management is a profession that encompasses multiple disciplines to ensure functionality of the built environment by integrating people, place, process and technology.
+          </p>
+        </div>
+        <div class="about-fm-competencies">
+        <p>
+            Our education focuses on the following 11 Core Competencies:
+            <ul>
+                <li>Communication</li>
+                <li>Emergency Preparedness and Business Continuity</li>
+                <li>Environmental Stewardship and Sustainability</li>
+                <li>Finance &amp; Business</li>
+              </ul>
+              <ul>
+                <li>Human Factors</li>
+                <li>Leadership and Strategy</li>
+                <li>Operations and Maintenance</li>
+                <li>Project Management</li>
+              </ul>
+              <ul>
+                <li>Quality</li>
+                <li>Real Estate and Property Management</li>
+                <li>Technology</li>
+            </ul>
+        </p>
+    </div>
+</div>
+
   <form role="search" action="<?php echo home_url('/'); ?>" method="get" class="search-form">
     <label class="screen-reader-text" for="s"><h2>Search Courses</h2></label>
     <br />
@@ -13,32 +46,4 @@ get_header(); ?>
         <button name="ifma_credential" value="all" type="submit" class="filter btn btn-default"><h3>View All Courses</h3></button>
     </div>
 </form>
-
-<div class="about-fm">
-        <div class="about-fm-definition">
-
-          <h4>Definition of Facility Management</h4>
-          <p>
-              Facility management, as defined by IFMA, is a profession that encompasses multiple disciplines to ensure functionality of the built environment by integrating people, place, process and technology.
-          </p>
-        </div>
-        <div class="about-fm-competencies">
-        <p>
-            We focus our education on the following 11 Core Competencies:
-            <ul>
-                <li>Communication</li>
-                <li>Emergency Preparedness and Business Continuity</li>
-                <li>Environmental Stewardship and Sustainability</li>
-                <li>Finance &amp; Business</li>
-                <li>Human Factors</li>
-                <li>Leadership and Strategy</li>
-                <li>Operations and Maintenance</li>
-                <li>Project Management</li>
-                <li>Quality</li>
-                <li>Real Estate and Property Management</li>
-                <li>Technology</li>
-            </ul>
-        </p>
-    </div>
-</div>
 <?php get_footer(); ?>
